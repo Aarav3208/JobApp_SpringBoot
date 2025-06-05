a1cc5f31a02a0296c3534cdbf1cf7bfd6504907c
Aarav Sharma <160886636+Aarav3208@users.noreply.github.com>
2025-06-05 12:20:03 +0530

    fixed main

---
+++ JobApp/main/java/com/tyrant/SpringJobProject/SpringJobProjectApplication.java
@@ -0,0 +1,15 @@
+package com.tyrant.SpringJobProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringJobProjectApplication.class, args);
+
+	}
+
+}
